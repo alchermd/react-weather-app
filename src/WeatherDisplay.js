ff0be7c08ea9d10dba046165ff8cd0eab57b5e28
alchermd <johnalcherdoloiras@gmail.com>
2017-07-23 03:49:59 +0800

    Display temperature with 2 decimal points.

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -3,7 +3,7 @@ import React from 'react'
 const WeatherDisplay = ({weather, tempUnit}) =>
     (<div>
         <h2>{weather.location}</h2>
-        <h3>{weather.temperature} {tempUnit}</h3>
+        <h3>{weather.temperature.toFixed(2)} {tempUnit}</h3>
         <p>{weather.description}</p>
     </div>)
 
