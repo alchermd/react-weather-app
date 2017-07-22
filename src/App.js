5a96c9a4177684a6648ef1ffd737de838294210c
alchermd <johnalcherdoloiras@gmail.com>
2017-07-23 03:26:18 +0800

    Render WeatherDisplay component with dummy weather data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import WeatherDisplay from './WeatherDisplay'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tempUnit: "C",
+      weather: {
+        location: "Malolos",
+        temperature: 24.5,
+        description: "Light Rain"
+      }
+    }
+  }
+
+
+
   render() {
     return (
       <div>
         <h1>Weather App</h1>
+        <hr/>
+        <WeatherDisplay weather={this.state.weather}/>
       </div>
     )
   }
