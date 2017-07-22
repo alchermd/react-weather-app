e2fc59a92ee7c85e7d6da2b2e93b00b4bb5c7258
alchermd <johnalcherdoloiras@gmail.com>
2017-07-23 04:32:53 +0800

    Add dynamic icons.

--- src/ToggleButton.js
+++ src/ToggleButton.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const ToggleButton = ({tempUnit, toggleUnit}) =>
-    (<div>
-        <button onClick={() => toggleUnit()}>{tempUnit === "C" ? "F" : "C"}</button>
+const ToggleButton = ({tempUnit, toggleUnit}) => {
+    const toggledUnit = tempUnit === "C" ? "F" : "C"
+    const icon = toggledUnit === "C" ? 
+        <i className="fa fa-toggle-off" aria-hidden="true"></i> :
+        <i className="fa fa-toggle-on" aria-hidden="true"></i>
+
+    return (<div>
+        <button onClick={() => toggleUnit()} className="btn btn-primary">
+            <strong>{toggledUnit}</strong> {icon}
+        </button>
     </div>)
+}
 
 export default ToggleButton
